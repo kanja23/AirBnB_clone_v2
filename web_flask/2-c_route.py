06b61a1f56a1eff7d5a2ae092eff285b880eccbd
kanja23 <martinkaranja92@gmail.com>
2023-07-30 07:33:18 -0700

    2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This is a simple Flask web application.
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Display 'Hello HBNB!'"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """Display 'HBNB'"""
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c_text(text):
+    """Display 'C' followed by the value of the text variable"""
+    text = text.replace('_', ' ')
+    return f'C {text}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
